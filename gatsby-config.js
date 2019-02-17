da2181f7ced84527b408dbe60a7f15e8fc588f64
Nirmalya Ghosh <nirmalya.email@gmail.com>
2019-02-17 23:45:03 +0530

    feat(app): Adds favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/static/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/static/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     {
