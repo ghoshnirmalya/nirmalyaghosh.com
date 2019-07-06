f804e81905e7ec6ac6326b81d570f1bff0033e81
Nirmalya Ghosh <nirmalya.email@gmail.com>
2019-07-06 15:47:13 +0530

    feat(a11y): Adds lang attribute to HTML tag

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -54,7 +54,9 @@ const IndexLayout: React.FC = ({ children }) => (
             { name: 'description', content: data.site.siteMetadata.description },
             { name: 'keywords', content: data.site.siteMetadata.keywords },
           ]}
-        />
+        >
+          <html lang="en" />
+        </Helmet>
         <Content>{children}</Content>
       </StyledLayout>
     )}
