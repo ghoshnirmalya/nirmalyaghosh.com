a18bde61afc4a601d0d6eaf53936a2c598a2ecc4
Nirmalya Ghosh <nirmalya.email@gmail.com>
2020-06-21 20:15:31 +0530

    fix: updates sitemap

--- lib/generate-sitemap.ts
+++ lib/generate-sitemap.ts
@@ -2,34 +2,36 @@
 
 const fs = require("fs");
 const globby = require("globby");
-const prettier = require("prettier");
 
 (async () => {
   const pages = await globby(["pages/**/*{.tsx,.mdx}", "!pages/_*.tsx"]);
   const sitemap = `
-        <?xml version="1.0" encoding="UTF-8"?>
-        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-            ${pages
-              .map((page) => {
-                const path = page
-                  .replace("pages", "")
-                  .replace("/index", "")
-                  .replace(".tsx", "")
-                  .replace(".mdx", "");
-                const route = path === "/index" ? "" : path;
-                return `
-                        <url>
-                            <loc>${`https://nirmalyaghosh.com${route}`}</loc>
-                        </url>
-                    `;
-              })
-              .join("")}
-        </urlset>
-    `;
+    <rss version="2.0">
+      <channel>
+        <title>Blog by Nirmalya Ghosh</title>
+        <link>https://www.nirmalyaghosh.com</link>
+        <description>Personal portfolio of Nirmalya Ghosh</description>
+        <language>en</language>
+        ${pages
+          .map((page) => {
+            const path = page
+              .replace("pages", "")
+              .replace("/index", "")
+              .replace(".tsx", "")
+              .replace(".mdx", "");
+            const route = path === "/index" ? "" : path;
+            return `
+              <item>
+                <link>
+                  ${`https://nirmalyaghosh.com${route}`}
+                </link>
+              </item>
+            `;
+          })
+          .join("")}
+      </channel>
+    </rss>
+  `;
 
-  const formatted = prettier.format(sitemap, {
-    parser: "html",
-  });
-
-  fs.writeFileSync("public/sitemap.xml", formatted);
+  fs.writeFileSync("public/sitemap.xml", sitemap);
 })();
