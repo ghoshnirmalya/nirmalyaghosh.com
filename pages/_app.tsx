4f1139b65454663f28878e771de5a298893561df
Nirmalya Ghosh <nirmalya.email@gmail.com>
2020-08-22 13:30:06 +0530

    refactor: removes unnecessary code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Head from "next/head";
 import siteConfig from "config/site";
-import { RecoilRoot } from "recoil";
 import { NextSeo } from "next-seo";
 import dynamic from "next/dynamic";
 import App from "next/app";
@@ -37,16 +36,6 @@ export function reportWebVitals({ id, name, label, value }) {
 }
 
 export default class MyApp extends App {
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {};
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-
-    return { pageProps };
-  }
-
   componentDidMount() {
     if (isProd) {
       const handleRouteChange = (url: string) => {
@@ -65,51 +54,42 @@ export default class MyApp extends App {
     const { Component, pageProps } = this.props;
 
     return (
-      <RecoilRoot>
-        <Layout>
-          <Head>
-            <meta
-              name="viewport"
-              content="width=device-width, initial-scale=1"
-            />
-            <link
-              rel="icon"
-              href={siteConfig.assets.favicon}
-              type="image/png"
-            />
-            <link
-              href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Noto+Serif:wght@700&display=swap"
-              rel="stylesheet"
-            />
-          </Head>
-          <NextSeo
-            title={`${siteConfig.details.title} - ${siteConfig.details.tagLine}`}
-            description={siteConfig.details.description}
-            twitter={{
-              handle: siteConfig.socialLinks.twitter,
-              site: siteConfig.socialLinks.twitter,
-              cardType: "summary_large_image",
-            }}
-            openGraph={{
-              url: siteConfig.details.url,
-              title: siteConfig.details.title,
-              description: siteConfig.details.description,
-              images: [
-                {
-                  url: siteConfig.assets.avatar,
-                  width: 800,
-                  height: 600,
-                  alt: siteConfig.details.title,
-                },
-              ],
-              site_name: siteConfig.details.title,
-              type: "website",
-              locale: "en_IE",
-            }}
+      <Layout>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href={siteConfig.assets.favicon} type="image/png" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Noto+Serif:wght@700&display=swap"
+            rel="stylesheet"
           />
-          <Component {...pageProps} />
-        </Layout>
-      </RecoilRoot>
+        </Head>
+        <NextSeo
+          title={`${siteConfig.details.title} - ${siteConfig.details.tagLine}`}
+          description={siteConfig.details.description}
+          twitter={{
+            handle: siteConfig.socialLinks.twitter,
+            site: siteConfig.socialLinks.twitter,
+            cardType: "summary_large_image",
+          }}
+          openGraph={{
+            url: siteConfig.details.url,
+            title: siteConfig.details.title,
+            description: siteConfig.details.description,
+            images: [
+              {
+                url: siteConfig.assets.avatar,
+                width: 800,
+                height: 600,
+                alt: siteConfig.details.title,
+              },
+            ],
+            site_name: siteConfig.details.title,
+            type: "website",
+            locale: "en_IE",
+          }}
+        />
+        <Component {...pageProps} />
+      </Layout>
     );
   }
 }
