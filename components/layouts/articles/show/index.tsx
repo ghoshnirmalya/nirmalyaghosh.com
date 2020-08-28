9b71a89aefcb9e4c1d8fa922b6aed9e578a059f1
Nirmalya Ghosh <nirmalya.email@gmail.com>
2020-08-28 10:15:55 +0530

    fix: fixes latest mdx plugin update

--- components/layouts/articles/show/index.tsx
+++ components/layouts/articles/show/index.tsx
@@ -8,7 +8,6 @@ import {
   useColorMode,
   Icon,
 } from "@chakra-ui/core";
-import IArticle from "types/article";
 import dynamic from "next/dynamic";
 import dayjs from "dayjs";
 import localizedFormat from "dayjs/plugin/localizedFormat";
@@ -21,78 +20,76 @@ const Footer = dynamic(import("components/footer"));
 
 dayjs.extend(localizedFormat);
 
-const Page = (article: IArticle) => {
-  return ({ children }) => {
-    const { colorMode } = useColorMode();
-    const sectionBgColor = { light: "gray.100", dark: "black" };
-    const sectionColor = { light: "black", dark: "gray.100" };
+const Page = ({ frontMatter, children }) => {
+  const { colorMode } = useColorMode();
+  const sectionBgColor = { light: "gray.100", dark: "black" };
+  const sectionColor = { light: "black", dark: "gray.100" };
 
-    const metaNode = (date: string, readingTime: string) => {
-      return (
-        <Stack spacing={4} isInline alignItems="center">
-          <Box>
-            <Text fontSize="xs">{dayjs(date).format("LL")}</Text>
-          </Box>
-          <Icon name="minus" size="12px" />
-          <Box>
-            <Text fontSize="xs">{readingTime}</Text>
-          </Box>
-        </Stack>
-      );
-    };
-
-    const titleNode = (title: string) => {
-      return (
-        <Heading as="h1" size="xl">
-          {title}
-        </Heading>
-      );
-    };
-
+  const metaNode = (date: string, readingTime: string) => {
     return (
-      <>
-        <NextSeo
-          title={`${article.title} | ${siteConfig.details.title}`}
-          description={article.description}
-          openGraph={{
-            url: `${siteConfig.details.url}/${article.__resourcePath.replace(
-              ".mdx",
-              "/"
-            )}`,
-            title: siteConfig.details.title,
-            description: article.description,
-            images: [
-              {
-                url: `${siteConfig.details.url}${siteConfig.assets.avatar}`,
-                width: 800,
-                height: 600,
-                alt: siteConfig.details.title,
-              },
-            ],
-            site_name: siteConfig.details.title,
-            type: "article",
-            locale: "en_IE",
-          }}
-        />
-        <SocialLinks />
-        <Navbar />
-        <Box bg={sectionBgColor[colorMode]} color={sectionColor[colorMode]}>
-          <Box maxW="3xl" mx="auto" px={4} py={8}>
-            <Grid templateColumns="1fr">
-              <Box maxW="100%" overflowX="hidden">
-                <Stack spacing={8}>
-                  {metaNode(article.date, article.readingTime.text)}
-                  {titleNode(article.title)}
-                  <Box className="article">{children}</Box>
-                </Stack>
-              </Box>
-            </Grid>
-          </Box>
+      <Stack spacing={4} isInline alignItems="center">
+        <Box>
+          <Text fontSize="xs">{dayjs(date).format("LL")}</Text>
         </Box>
-        <Footer />
-      </>
+        <Icon name="minus" size="12px" />
+        <Box>
+          <Text fontSize="xs">{readingTime}</Text>
+        </Box>
+      </Stack>
     );
   };
+
+  const titleNode = (title: string) => {
+    return (
+      <Heading as="h1" size="xl">
+        {title}
+      </Heading>
+    );
+  };
+
+  return (
+    <>
+      <NextSeo
+        title={`${frontMatter.title} | ${siteConfig.details.title}`}
+        description={frontMatter.description}
+        openGraph={{
+          url: `${siteConfig.details.url}/${frontMatter.__resourcePath.replace(
+            ".mdx",
+            "/"
+          )}`,
+          title: siteConfig.details.title,
+          description: frontMatter.description,
+          images: [
+            {
+              url: `${siteConfig.details.url}${siteConfig.assets.avatar}`,
+              width: 800,
+              height: 600,
+              alt: siteConfig.details.title,
+            },
+          ],
+          site_name: siteConfig.details.title,
+          type: "article",
+          locale: "en_IE",
+        }}
+      />
+      <SocialLinks />
+      <Navbar />
+      <Box bg={sectionBgColor[colorMode]} color={sectionColor[colorMode]}>
+        <Box maxW="3xl" mx="auto" px={4} py={8}>
+          <Grid templateColumns="1fr">
+            <Box maxW="100%" overflowX="hidden">
+              <Stack spacing={8}>
+                {metaNode(frontMatter.date, frontMatter.readingTime.text)}
+                {titleNode(frontMatter.title)}
+                <Box className="article">{children}</Box>
+              </Stack>
+            </Box>
+          </Grid>
+        </Box>
+      </Box>
+      <Footer />
+    </>
+  );
 };
 
 export default Page;
