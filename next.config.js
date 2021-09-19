5efc93ee3056144d1f756ea8d1161261d14cdfa8
Nirmalya Ghosh <nirmalya.email@gmail.com>
2021-09-19 17:27:10 +0530

    refactor: Adds Husky

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+
 module.exports = {
   reactStrictMode: true,
-  productionBrowserSourceMaps: true,
   experimental: {
     esmExternals: true,
   },
