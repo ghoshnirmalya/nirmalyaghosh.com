1aa7a812beeb5bbd7270a51b990952b59fcd7034
Nirmalya Ghosh <nirmalya.email@gmail.com>
2021-10-09 10:38:08 +0530

    refactor: Updates OG data

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,9 @@
 const siteConfig = {
   details: {
     title: "Nirmalya Ghosh",
-    tagLine: "Designer and Developer",
-    description: "Personal portfolio of Nirmalya Ghosh",
+    tagLine: "Sofware Developer",
+    description:
+      "Articles and guides about JavaScript, CSS and Web Development from a software engineer.",
     url: "https://www.nirmalyaghosh.com",
   },
   assets: {
