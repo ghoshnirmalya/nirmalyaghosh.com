abe5ad74c4640aefa87817776b73938ed5867b9f
Nirmalya Ghosh <nirmalya.email@gmail.com>
2021-10-15 00:12:37 +0530

    refactor: Uses Preact

--- next.config.js
+++ next.config.js
@@ -9,4 +9,15 @@ module.exports = withContentlayer()({
   experimental: {
     esmExternals: true,
   },
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+
+    return config;
+  },
 });
