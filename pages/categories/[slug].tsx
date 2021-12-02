b2c0d5fc286cd5e27b4eb9fc5518405bcfb1b3b2
Nirmalya Ghosh <nirmalya.email@gmail.com>
2021-12-02 23:11:48 +0530

    refactor: removes unnecessary data from CSR

--- pages/categories/[slug].tsx
+++ pages/categories/[slug].tsx
@@ -1,34 +1,17 @@
+import { Article } from ".contentlayer/types";
 import Page from "components/pages/categories/base";
 import { getAllArticlesWhichBelongToCurrentSlug } from "lib/get-articles-data";
 import { getAllCategories } from "lib/get-categories-data";
-import {
-  GetStaticPaths,
-  GetStaticProps,
-  GetStaticPropsContext,
-  NextPage,
-} from "next";
-import { ParsedUrlQuery } from "querystring";
+import pick from "lodash/pick";
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 
 interface IProps {
-  params: ParsedUrlQuery;
+  articles: Article[];
+  currentCategory: string;
 }
 
-const TagsListingPage: NextPage<IProps> = ({ params }) => {
-  const articles = getAllArticlesWhichBelongToCurrentSlug(params, "categories");
-  const currentCategory = params?.slug as string;
-
+const TagsListingPage: NextPage<IProps> = ({ articles, currentCategory }) => {
   return <Page articles={articles} currentCategory={currentCategory} />;
-};
-
-export const getStaticProps: GetStaticProps = async ({
-  params,
-}: GetStaticPropsContext) => {
-  return {
-    props: {
-      params,
-      currentCategory: params?.slug,
-    },
-  };
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
@@ -41,6 +24,20 @@ export const getStaticPaths: GetStaticPaths = async () => {
       };
     }),
     fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const articles = getAllArticlesWhichBelongToCurrentSlug(
+    params,
+    "categories"
+  ).map((articles) => pick(articles, ["date", "description", "title", "slug"]));
+
+  return {
+    props: {
+      articles,
+      currentCategory: params?.slug,
+    },
   };
 };
 
