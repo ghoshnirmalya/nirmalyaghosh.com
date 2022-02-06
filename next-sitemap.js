2496fdd6580ff7cc92f0d19e30f76ed8cc5d1b3f
Nirmalya Ghosh <nirmalya.email@gmail.com>
2022-02-06 23:35:23 +0530

    feat: adds sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,21 @@
+module.exports = {
+  siteUrl: "https://nirmalyaghosh.com",
+  generateRobotsTxt: true,
+  changefreq: "daily",
+  priority: 1,
+  transform: async (config, path) => {
+    const ignoredPaths = ["/500", "/404"];
+
+    if (ignoredPaths.indexOf(path) > -1) {
+      return null;
+    }
+
+    return {
+      loc: path,
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    };
+  },
+};
