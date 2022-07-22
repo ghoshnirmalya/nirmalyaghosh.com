68e9a2a38799a4925093fcea2120b76c2fddbf9b
Nirmalya Ghosh <nirmalya.email@gmail.com>
2022-07-22 23:36:14 +0530

    refactor: Adds ENV key

--- utils/notion-client.ts
+++ utils/notion-client.ts
@@ -1,7 +1,7 @@
 import { Client } from "@notionhq/client";
 
 const notionClient = new Client({
-  auth: "secret_aY5vOqDDl891S87X4t26b7uFM8cOI7QPce2WITWhnBB",
+  auth: process.env.NOTION_API_KEY,
 });
 
 export default notionClient;
