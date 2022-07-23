f5ee8c61a175410930fc2b5bc03d491e1cb9530e
Nirmalya Ghosh <nirmalya.email@gmail.com>
2022-07-23 19:13:18 +0530

    refactor:  Increases Notion API timeout

--- utils/notion-client.ts
+++ utils/notion-client.ts
@@ -1,7 +1,8 @@
-import { Client } from "@notionhq/client";
+import { Client, LogLevel } from "@notionhq/client";
 
 const notionClient = new Client({
   auth: process.env.NOTION_API_KEY,
+  timeoutMs: 60_000 * 60,
 });
 
 export default notionClient;
