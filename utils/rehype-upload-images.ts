7cf4fa6e5ed7acfe93e6f31204cd452f95edd0a8
Nirmalya Ghosh <nirmalya.email@gmail.com>
2022-12-24 22:26:51 +0530

    feat: Uses ImageKit service

---
+++ utils/rehype-upload-images.ts
@@ -0,0 +1,58 @@
+import ImageKit from "imagekit";
+import MDAST from "mdast";
+import { IArticle } from "types/article";
+import type { VisitorResult } from "unist-util-visit";
+import { visit } from "unist-util-visit";
+
+var imagekit = new ImageKit({
+  publicKey: process.env.IMAGEKIT_PUBLIC_API_KEY,
+  privateKey: process.env.IMAGEKIT_PRIVATE_API_KEY,
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
+});
+
+interface Node extends MDAST.Parent {
+  tagName: string;
+  properties: { [key: string]: any };
+}
+
+function rehypeUploadImages(articleSlug: IArticle["slug"]) {
+  return async (tree: MDAST.Root) => {
+    const uploadImagePromises: any[] = [];
+    let fileNumber = 1;
+
+    const visitor = (node: Node): VisitorResult => {
+      if (node.tagName === "img") {
+        const src = node.properties.src;
+        const fileName = `${articleSlug}-${fileNumber}-${Date.now()}`;
+
+        const uploadImagePromise = imagekit.upload(
+          {
+            file: src,
+            fileName: `${fileName}.png`,
+            useUniqueFileName: false,
+          },
+          function (error: unknown) {
+            if (error) {
+              console.log(error);
+            }
+          }
+        );
+
+        uploadImagePromises.push(uploadImagePromise);
+
+        node.properties.src = `${process.env.IMAGEKIT_URL_ENDPOINT}/${fileName}.png`;
+        node.properties.loading = "lazy";
+
+        fileNumber++;
+      }
+    };
+
+    visit(tree, "element", visitor);
+
+    await Promise.all(uploadImagePromises);
+
+    return null;
+  };
+}
+
+export default rehypeUploadImages;
