e81f5ad2adcd0caee044bdc9016ef901f0f56701
Nirmalya Ghosh <nirmalya.email@gmail.com>
2022-12-31 16:28:18 +0530

    feat: Adds Vercel audiences

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
-import { Box } from "@chakra-ui/react";
+import { Analytics } from "@vercel/analytics/react";
 import siteConfig from "config/site";
+import "focus-visible/dist/focus-visible";
 import { initGA } from "lib/google-analytics";
 import isProduction from "lib/is-production";
 import { NextSeo } from "next-seo";
@@ -7,7 +8,6 @@ import { AppProps } from "next/app";
 import dynamic from "next/dynamic";
 import Head from "next/head";
 import { useEffect } from "react";
-import "focus-visible/dist/focus-visible";
 
 const Layout = dynamic(
   () => import(/* webpackChunkName: "Layouts" */ "components/layouts")
@@ -56,6 +56,7 @@ const PortfolioApp = ({ Component, pageProps }: AppProps) => {
       />
       <Navbar />
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   );
 };
