b3f95341730acddf752f8fbbce545e59a53414f9
Nirmalya Ghosh <nirmalya.email@gmail.com>
2023-05-29 12:16:00 +0530

    chore: updates all outdated packages

--- next.config.js
+++ next.config.js
@@ -1,18 +1,8 @@
-/**
- * @type {import('next').NextConfig}
- */
-
 const { withContentlayer } = require("next-contentlayer");
 
-module.exports = withContentlayer({
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: "/concepts/:slug",
-        destination: "/articles/:slug",
-        permanent: true,
-      },
-    ];
-  },
-});
+};
+
+module.exports = withContentlayer(nextConfig);
